54027c652e5998c29548ffa435ff30b07e910432
Brian <riceb53@gmail.com>
2018-11-15 19:13:03 -0600

    add momentjs

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var moment = require('moment');
+var math = require('mathjs');
 
+console.log(math.sqrt(-4));
 console.log('i\'m in index.js');
 
 console.log(moment("20241031", "YYYYMMDD").fromNow());
