7ceac70a64826d6129f6b5d6a4ee9ce1b4ea5e00
Brian <riceb53@gmail.com>
2018-11-15 20:16:10 -0600

    add webpack server

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var moment = require('moment');
 var math = require('mathjs');
 
-console.log(math.sqrt(-4));
-console.log('i\'m in index.js');
 
-console.log(moment("20241031", "YYYYMMDD").fromNow());
+console.log('i\'m in index.js...NOT');
 
-var nums = [1,23,4123,4,123,54,55,55];
+console.log(moment("20121031", "YYYYMMDD").fromNow());
+
+var nums = [1,23,4,123,54,55,55];
 var fives = [];
 
 nums.forEach(function(v) {
